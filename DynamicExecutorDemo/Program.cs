6f5d3b0bb3a98fb035768b1a1fa156a4551a7e07
dieron <dieron@gmail.com>
2024-11-23 12:58:13 -0800

    Added void returning lambdas support

--- DynamicExecutorDemo/Program.cs
+++ DynamicExecutorDemo/Program.cs
@@ -1,130 +1,156 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 public record MethodParameterDto
 {
-	public string TypeName { get; init; }
-	public object Value { get; init; }
+    public string TypeName { get; init; }
+    public object Value { get; init; }
 }
 
 public record MethodCallDto
 {
-	public string InterfaceType { get; init; }
-	public string MethodName { get; init; }
-	public MethodParameterDto[] Arguments { get; init; }
+    public string InterfaceType { get; init; }
+    public string MethodName { get; init; }
+    public MethodParameterDto[] Arguments { get; init; }
 }
 
 public static class DynamicExecutor
 {
-	public static TResult Execute<TInterface, TResult>(Expression<Func<TInterface, TResult>> expression)
-	{
-		if (expression.Body is MethodCallExpression methodCall)
-		{
-			var parameters = methodCall.Method.GetParameters();
-			var argumentValues = GetArgumentValues(methodCall.Arguments);
+    public static TResult Execute<TInterface, TResult>(Expression<Func<TInterface, TResult>> expression)
+    {
+        if (expression.Body is MethodCallExpression methodCall)
+        {
+            var dto = CreateMethodCallDto<TInterface>(methodCall);
+            var json = SerializeMethodCall(dto);
 
-			// Serialize with type information
-			var dto = new MethodCallDto
-			{
-				InterfaceType = typeof(TInterface).AssemblyQualifiedName,
-				MethodName = methodCall.Method.Name,
-				Arguments = parameters.Select((p, i) => new MethodParameterDto
-				{
-					TypeName = p.ParameterType.AssemblyQualifiedName,
-					Value = argumentValues[i]
-				}).ToArray()
-			};
+            Console.WriteLine($"Serialized call: {json}");
 
-			var options = new JsonSerializerOptions
-			{
-				WriteIndented = true
-			};
-			var json = JsonSerializer.Serialize(dto, options);
-			Console.WriteLine($"Serialized call: {json}");
+            var deserializedDto = DeserializeMethodCall(json);
+            return (TResult)InvokeMethod<TInterface>(deserializedDto);
+        }
+        throw new ArgumentException("Expression should be a method call.");
+    }
 
-			// Deserialize and convert types
-			var deserializedDto = JsonSerializer.Deserialize<MethodCallDto>(json);
-			var type = Type.GetType(deserializedDto.InterfaceType);
-			var instance = Factory.Create<TInterface>();
-			var methodInfo = type.GetMethod(deserializedDto.MethodName);
+    public static void Execute<TInterface>(Expression<Action<TInterface>> expression)
+    {
+        if (expression.Body is MethodCallExpression methodCall)
+        {
+            var dto = CreateMethodCallDto<TInterface>(methodCall);
+            var json = SerializeMethodCall(dto);
 
-			// Convert arguments to correct types
-			var typedArguments = deserializedDto.Arguments
-				.Select((arg, i) =>
-				{
-					var paramType = Type.GetType(arg.TypeName);
-					return JsonSerializer.Deserialize(
-						JsonSerializer.Serialize(arg.Value),
-						paramType
-					);
-				}).ToArray();
+            Console.WriteLine($"Serialized call: {json}");
 
-			return (TResult)methodInfo.Invoke(instance, typedArguments);
-		}
-		throw new ArgumentException("Expression should be a method call.");
-	}
+            var deserializedDto = DeserializeMethodCall(json);
+            InvokeMethod<TInterface>(deserializedDto);
+            return;
+        }
+        throw new ArgumentException("Expression should be a method call.");
+    }
 
+    private static MethodCallDto CreateMethodCallDto<TInterface>(MethodCallExpression methodCall)
+    {
+        var parameters = methodCall.Method.GetParameters();
+        var argumentValues = GetArgumentValues(methodCall.Arguments);
 
-	private static object[] GetArgumentValues(System.Collections.ObjectModel.ReadOnlyCollection<Expression> arguments)
-	{
-		var argumentValues = new object[arguments.Count];
-		for (int i = 0; i < arguments.Count; i++)
-		{
-			var lambda = Expression.Lambda(arguments[i]);
-			var compiled = lambda.Compile();
-			argumentValues[i] = compiled.DynamicInvoke();
-		}
-		return argumentValues;
-	}
+        return new MethodCallDto
+        {
+            InterfaceType = typeof(TInterface).AssemblyQualifiedName,
+            MethodName = methodCall.Method.Name,
+            Arguments = parameters.Select((p, i) => new MethodParameterDto
+            {
+                TypeName = p.ParameterType.AssemblyQualifiedName,
+                Value = argumentValues[i]
+            }).ToArray()
+        };
+    }
 
+    private static string SerializeMethodCall(MethodCallDto dto)
+    {
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+        return JsonSerializer.Serialize(dto, options);
+    }
 
-	// Similar changes for void Execute<TInterface> method...
+    private static MethodCallDto DeserializeMethodCall(string json)
+    {
+        return JsonSerializer.Deserialize<MethodCallDto>(json);
+    }
+
+    private static object InvokeMethod<TInterface>(MethodCallDto dto)
+    {
+        var type = Type.GetType(dto.InterfaceType);
+        var methodInfo = type.GetMethod(dto.MethodName);
+        var arguments = dto.Arguments.Select(arg =>
+        {
+            var paramType = Type.GetType(arg.TypeName);
+            var json = JsonSerializer.Serialize(arg.Value);
+            return JsonSerializer.Deserialize(json, paramType);
+        }).ToArray();
+
+        var instance = Factory.Create<TInterface>();
+        return methodInfo.Invoke(instance, arguments);
+    }
+
+    private static object[] GetArgumentValues(ReadOnlyCollection<Expression> arguments)
+    {
+        var argumentValues = new object[arguments.Count];
+        for (int i = 0; i < arguments.Count; i++)
+        {
+            var lambda = Expression.Lambda(arguments[i]);
+            var compiled = lambda.Compile();
+            argumentValues[i] = compiled.DynamicInvoke();
+        }
+        return argumentValues;
+    }
 }
 
 public static class Factory
 {
-	public static TInterface Create<TInterface>()
-	{
-		// Implement your factory logic here. For demo purposes, use Activator.
-		if (typeof(TInterface) == typeof(IExample))
-		{
-			return (TInterface)(object)new ExampleImplementation();
-		}
-		throw new NotImplementedException($"Factory doesn't know how to create type {typeof(TInterface).Name}");
-	}
+    public static TInterface Create<TInterface>()
+    {
+        if (typeof(TInterface) == typeof(IExample))
+        {
+            return (TInterface)(object)new ExampleImplementation();
+        }
+        throw new InvalidOperationException($"No implementation registered for {typeof(TInterface).Name}");
+    }
 }
 
 public interface IExample
 {
-	int Add(int x, int y);
-	void Print(string message);
+    int Add(int x, int y);
+    void Print(string message);
 }
 
 public class ExampleImplementation : IExample
 {
-	public int Add(int x, int y)
-	{
-		return x + y;
-	}
+    public int Add(int x, int y)
+    {
+        return x + y;
+    }
 
-	public void Print(string message)
-	{
-		Console.WriteLine(message);
-	}
+    public void Print(string message)
+    {
+        Console.WriteLine(message);
+    }
 }
 
-
-
-// Demo usage
+// Usage
 class Program
 {
-	static void Main(string[] args)
-	{
-		var result = DynamicExecutor.Execute<IExample, int>(x => x.Add(5, 3));
-		Console.WriteLine($"Result: {result}");
+    static void Main(string[] args)
+    {
+        // For methods that return a value
+        var result = DynamicExecutor.Execute<IExample, int>(x => x.Add(5, 3));
+        Console.WriteLine($"Result: {result}");
 
-		// DynamicExecutor.Execute<IExample>(x => x.Print("Hello, World!"));
-	}
+        // For methods that return void
+        DynamicExecutor.Execute<IExample>(x => x.Print("Hello, World!"));
+    }
 }
